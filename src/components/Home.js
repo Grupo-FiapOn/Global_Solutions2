8c843facf93e0a0b8f6eb92af221c9ec0a30ecb7
alevaiano <rm551497@fiap.com.br>
2023-11-22 19:38:16 -0300

    base do formulário

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,44 @@
-//importar imagens
+//importar imagens:
 //import nomedaimagem from "./../imagens/nomedaimagem.png
 
 function Home(){
 
     return(
-        <div id="Home">
-            <h2>HOME</h2>
-            <p>Conteúdo</p>
-            {/*<img src={nomedaimagem} alt='descrição da imagem' /> */}
+        <div>
+            <div id="Home">
+                <h2>HOME</h2>
+                <p>Conteúdo</p>
+                {/*para inserir a imagem:
+                <img src={nomedaimagem} alt='descrição da imagem' /> 
+                obs: a imagem precisa estar dentro da pasta imagens*/}
+            </div>
+
+            <div id="Formulario">
+                <form>
+                    <fieldset>
+                        <legend>Fale Conosco</legend>
+                        <label>Nome:
+                            <input type="text" name="nome"/>
+                        </label>
+                        <label>E-mail:
+                            <input type="text" name="email"/>
+                        </label>
+                        <label>Mensagem:
+                            <input type="text" name="mensagem"/>
+                        </label>
+                        <button type="submit">Enviar</button>
+                       {/* alert("Sua mensagem foi enviada com sucesso!") 
+                       também preciso validar o formulário, máximo de letras no nome e apenas text
+                       email tem que ter @ */}
+                    </fieldset>
+                </form>
+            </div>
+            
         </div>
     )
 
 }
 
 export default Home
+
+//para inserir no App.js eu jogo lá 1) import 2) Dentro do return(<nomedoarquivo/>)
