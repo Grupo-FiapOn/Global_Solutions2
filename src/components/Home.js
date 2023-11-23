30e34c51f2422dd004cb460d18a647284f8d642a
alevaiano <rm551497@fiap.com.br>
2023-11-22 21:02:11 -0300

    formulário usando HookForm

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,32 @@
 //importar imagens:
 //import nomedaimagem from "./../imagens/nomedaimagem.png
 
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+//validacoes para meu formulario
+const schema = yup.object({
+    nome: yup.string().min(5,"O Nome deve conter no mínimo 5 caracteres!").required("Preencha o seu Nome"),
+    email: yup.string().email("Digite um e-mail válido!").required("Preencha o seu e-mail"),
+    mensagem: yup.string().min(10,"A Mensagem deve conter pelo menos 10 caracteres!").required("Escreva sua mensagem"),
+    }).required();
+
 function Home(){
 
+//usando as ferramentas do useForm. Aqui conecto a const schema que criei com o hookForm
+    const{ register, handleSubmit, formState: {errors}} = useForm({
+        resolver: yupResolver(schema)
+    })
+
+//para armazenar mais de um dado preenchido no formulário
+    const [listaUsuario, setListaUsuario] = useState([])
+//a função para chamar essa lista
+    function inserirUsuario(usuario){
+        setListaUsuario([...listaUsuario, usuario])
+    }
+    
     return(
         <div>
             <div id="Home">
@@ -14,26 +38,37 @@ function Home(){
             </div>
 
             <div id="Formulario">
-                <form>
+                <form onSubmit={handleSubmit(inserirUsuario)}>
                     <fieldset>
                         <legend>Fale Conosco</legend>
                         <label>Nome:
-                            <input type="text" name="nome"/>
+                            <input type="text" {...register("nome")}/>
+                            <span>{errors.nome?.message}</span> {/*aqui eu chamo a validação que fiz no const schema = yup.object*/}
                         </label>
                         <label>E-mail:
-                            <input type="text" name="email"/>
+                            <input type="text" {...register("email")}/>
+                            <span>{errors.email?.message}</span>
                         </label>
                         <label>Mensagem:
-                            <input type="text" name="mensagem"/>
+                            <input type="text" {...register("mensagem")}/>
+                            <span>{errors.mensagem?.message}</span>
                         </label>
                         <button type="submit">Enviar</button>
                        {/* alert("Sua mensagem foi enviada com sucesso!") 
-                       também preciso validar o formulário, máximo de letras no nome e apenas text
-                       email tem que ter @ */}
+                       precisa armazenar esses dados num .json? api?*/}
                     </fieldset>
                 </form>
+                <div id="armazenarUsuario">
+                    {listaUsuario.map((usu,index)=>
+                        <div id="dados" key={index}>
+                            <p>Nome: {usu.nome}</p>
+                            <p>Email: {usu.email}</p>
+                            <p>Mensagem: {usu.mensagem}</p>
+                        </div>
+                    )}
+                </div>
             </div>
-            
+
         </div>
     )
 
