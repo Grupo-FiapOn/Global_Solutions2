cae7f64be7322a0573f8e743fe1bcbba9a167bc4
alevaiano <rm551497@fiap.com.br>
2023-11-23 02:40:42 -0300

    conteudo doencas nao transmissiveis

--- src/components/PrevencaoETratamentoDeDoencasNaoTransmissiveis.js
+++ src/components/PrevencaoETratamentoDeDoencasNaoTransmissiveis.js
@@ -6,7 +6,36 @@ function PrevencaoETratamentoDeDoencasNaoTransmissiveis(){
     return(
         <div id="PrevencaoETratamentoDeDoencasNaoTransmissiveis">
             <h2>Prevenção E Tratamento De Doenças Não Transmissíveis</h2>
-            <p>Conteúdo</p>
+            <h3> Um Guia Rápido:</h3>
+            <p>As doenças não transmissíveis são responsáveis por 72% das mortes no Brasil! 
+            Os impactos negativos na saúde física, mental e na rotina alimentar aumentam esses riscos.</p>
+
+            <p>O tabagismo, consumo excessivo de álcool, dietas inadequadas e falta de atividade física 
+            representam 51% das mortes por doenças crônicas.</p>
+
+            <h5>O Controle dessas doenças depende de hábitos e ações que melhoram a qualidade de vida</h5> 
+
+           <p> 
+            <h4>Importância da Alimentação e Atividade Física</h4>
+
+            <ul>A Atividade Física previne:
+                <li> 52% da hipertensão arterial</li>
+                <li> 48% das doenças cardiovasculares</li>
+                <li> 16% do câncer de cólon</li>
+                <li> 14% do diabetes</li>
+                <li> 10% do câncer de mama</li>
+            </ul>
+            <ul>Uma Alimentação Balanceada previne:
+                <li> 61% do diabetes tipo 2</li>
+                <li> 34% do câncer de endometriose</li>
+                <li> 30% das doenças da vesícula biliar</li>
+                <li> 24% da osteoartrose</li>
+                <li> 17% das doenças cardiovasculares</li>
+                <li> 17% da hipertensão arterial</li>
+                <li> 11% do câncer de intestino</li>
+                <li> 11% do câncer de mama</li>
+            </ul>
+            </p>
             {/*para inserir a imagem:
             <img src={nomedaimagem} alt='descrição da imagem' /> 
             obs: a imagem precisa estar dentro da pasta imagens*/}
