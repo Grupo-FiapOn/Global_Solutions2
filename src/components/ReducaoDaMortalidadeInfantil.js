5b6e97ee2937841bed36b165c954914633576891
alevaiano <rm551497@fiap.com.br>
2023-11-23 21:02:51 -0300

    mortalidade infantil

--- src/components/ReducaoDaMortalidadeInfantil.js
+++ src/components/ReducaoDaMortalidadeInfantil.js
@@ -5,13 +5,51 @@ function ReducaoDaMortalidadeInfantil(){
     return(
         <div className="ReducaoDaMortalidadeInfantil">
             <h2>Redução Da Mortalidade Infantil</h2>
-            <p>Conteúdo</p>
+            <h3>Dados</h3>
+            <p> 
+                <ul>
+                    <li>Segundo dados da Organização Mundial da Saúde (OMS), em 2019, a taxa global de mortalidade infantil era de aproximadamente 38 mortes por 1.000 nascidos vivos.</li>
+                    <li>As principais causas de morte em crianças menores de 5 anos são: complicações de parto prematuro, asfixia/trauma durante o parto, pneumonia, diarreia e malária, todas as quais podem ser prevenidas ou tratadas com acesso a intervenções acessíveis em saúde e saneamento.</li>
+                    <li>A comunidade internacional estabeleceu o Objetivo de Desenvolvimento Sustentável (ODS) número 3, que inclui a meta de reduzir a taxa de mortalidade neonatal e infantil.</li>
+                    <li>Desde 1990, a taxa global de mortalidade de menores de 5 anos caiu 60%, de 93 mortes por 1000 recém-nascidos em 1990 para 37 em 2020. Isto equivale a 1 em 11 crianças que morrem antes de completar 5 anos em 1990, em comparação com 1 em 27 em 2020.
+                    </li>
+                </ul>
+                    <br></br>
+                Essas informações fornecem uma visão geral em relação à mortalidade infantil, de modo a conscientizar na busca de soluções para tal problemática, já que essas taxas são indicadores não somente da saúde infantil, mas da saúde de uma sociedade.
+            </p>
+            <img src="imagens/foto3.avif" title="" alt="" width="60%"/>
+            <h3>Principais Desafios e Causas</h3>
+            <p>
+            A mortalidade infantil é uma problemática complexa, a qual se relaciona com uma combinação de fatores sociais, econômicos e de saúde. 
+            A iniciativa global de combatê-la tem mostrado avanços ao longo das últimas décadas, mas essa questão ainda persiste e deve ser solucionada. A seguir estão os desafios enfrentados quanto à mortalidade infantil:<br></br>
+                <ul>
+                    <li>Desnutrição</li>
+                    <li>Complicações no Parto</li>
+                    <li>Fatores Socioeconômicos</li>
+                    <li>Gravidez na Adolescência</li>
+                    <li>Baixa Cobertura de Vacinação</li>
+                    <li>Falta de Educação Materna</li>
+                    <li>Doenças Infecciosas</li>
+                    <li>Condições Sanitárias Precárias</li>
+                </ul>
+            </p>
+            <h3>Possíveis Soluções</h3>
+            <p>
+                É notório que se faz necessário a iniciativa da cooperação generalizada em torno desse tema, uma vez que ao proteger a vida das crianças estaremos protegendo o futuro da sociedade e, por consequência, seu sucesso. Essas são algumas medidas que podem ser tomadas em torno disso:
+                <ul>
+                    <li>Melhoria no Acesso à Saúde</li>
+                    <li>Acesso Universal a Vacinas</li>
+                    <li>Cooperação Global</li>
+                    <li>Promoção de Práticas Culturais Saudáveis</li>
+                    <li>Inovações em Tecnologia da Saúde</li>
+                    <li>Combate Ativo contra a Desnutrição</li>
+                </ul>
+            </p>
         
         </div>
-    )
+        
+        )
 
 }
 
 export default ReducaoDaMortalidadeInfantil
-
-//para inserir no App.js eu jogo lá 1) import 2) Dentro do return(<nomedoarquivo/>)
